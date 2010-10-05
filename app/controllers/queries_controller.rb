af4eacb104e75bfd194bc3aab45c60d2cb7fb376
Alan Andrade <gallito7@gmail.com>
2010-10-05 09:56:27 -0500

    Code moved to query model. TODO: Need a better way to get the code look cleaner and easy to read. Iterations added to Relevance Feedback. TODO: Check is works fine :S

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -17,10 +17,11 @@ layout  'docs'
     
     @query_id     = params[:query_id]
     @metodo       = params[:metodo]  
+    @iterations   = params[:iterations]
     @query_terms  = Query.find_all_by_query_id(@query_id).map{|q| q.term } # palabras de la consulta
     query_terms_ids = @query_terms.map(&:id)
     @query_terms  = @query_terms.map(&:term)   
-    @results = Query.query(@query_id, @metodo)      
+    @results      = Query.query(@query_id, @metodo)      
     
     # Se recaba la informacion de los documentos relevantes en @answers. Se obtienen los ids de los documentos que resultaron.
     @answers          = Answer.find_all_by_query_id(@query_id)
@@ -28,8 +29,8 @@ layout  'docs'
     
     # If que permite usar o no relevancia dependiendo de la decision del usuario.
     if params[:feedback]      
-      @results = Query.query_with_feedback(@query_id, @metodo, @results, @relevant_doc_ids, query_terms_ids, @query_terms)        
-    end #end if feedback   
+      @results = Query.query_with_feedback(@query_id, @iterations, @metodo)        
+    end
     
   end
 
