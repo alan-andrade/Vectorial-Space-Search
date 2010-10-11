f6e27db53fb52385fd4df673b54021f37928ef56
Alan Andrade <gallito7@gmail.com>
2010-10-10 20:08:51 -0500

    Clusterize almost complete. Need to calculate the centroid of each cluster

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -3,8 +3,8 @@ class Cluster < ActiveRecord::Base
   # Documents in one cluster logic
   ##
   
-  has_many  :cluster_docs
-  has_many  :docs, :through =>  :cluster_docs
+  has_many  :cluster_docs,  :dependent  =>  :destroy
+  has_many  :docs,          :through    =>  :cluster_docs
   
   ##
   # Self Referential logic
@@ -20,18 +20,68 @@ class Cluster < ActiveRecord::Base
                         :dependent    =>  :destroy
   has_one   :parent   , :through      =>  :son_of                      
   
-  ##
-  # clustrize Implementation
-  ##
+  ###
+  # Clustrize Implementation
+  ###
   def self.clusterize
     ##
     # One cluster per Document
     ##
-    Doc.all.each{|document| Cluster.create.docs << document }
+    Doc.all.each{|document| (Cluster.create.docs << document) unless document.cluster }
+    
+    ###
+    # Using Complete Binding. The min of similarities 
+    ###
+    similarity_matrix = SimilarityMatrix.find :all, 
+                                              :conditions => ["similarity > ?" , 0], 
+                                              :order      => 'similarity'
+    ###
+    # Use the order from similarity_matrix. From the min to max, we start to cluster.
+    # !! EXPERIMENTAL !!
+    ###
+    
+    for similarity in similarity_matrix
+      first_doc_id  = similarity.x
+      second_doc_id = similarity.y
+      join_from_docs(first_doc_id,  second_doc_id)      
+      #similarity.destroy
+    end
     
     ##
-    # Using Unique Binding. The min of similarities 
-    ##
-    SimilarityMatrix.minimum('similarity', :conditions=>"similarity > 0")
+    # Finally, Calculate the centroid of each Cluster
+    ## 
+    
+    
+    
+  end
+  
+  private
+  
+  def self.join_from_docs(first_doc_id,second_doc_id)
+    first_document  = Doc.find(first_doc_id,  :include  =>  :cluster)
+    first_cluster   = first_document.cluster
+    
+    second_document = Doc.find(second_doc_id, :include  =>  :cluster)    
+    second_cluster  = second_document.cluster
+    
+    join_from_clusters(first_cluster, second_cluster)
+  end
+  
+  def self.join_from_clusters(first_cluster, second_cluster)
+    first_cluster_documents   = first_cluster.docs.count
+    second_cluster_documents  = second_cluster.docs.count
+    
+    if first_cluster_documents == 1 and second_cluster_documents == 1
+      ##
+      # Soft Join: Join the document INTO one cluster.
+      ##
+      first_cluster.docs << second_cluster.docs
+      second_cluster.destroy
+    else
+      ##
+      # Hard Join: Join clusters into Clusters.
+      ##    
+      Cluster.create.childs << first_cluster << second_cluster
+    end
   end
 end
